9a310940c02d25a0c9fd49512edfbc3a4a66f8ca
RobincYoung <37380230+RobincYoung@users.noreply.github.com>
2018-03-14 20:30:46 +0000

    added comments
    added comments to make it more readable

--- maincode.ino
+++ maincode.ino
@@ -1,5 +1,6 @@
 #include <ESP8266WiFi.h>
-char* names[] = {"Im watching you", "secret camera #%d", "Hi there", "BTE-IT", "I see you","MI5reconvanID#%d","loading..." , "no free wifi because Tories", "yell CARROT for password", "FBIvanID#%d", "Fellowship of the Ping", "get off my LAN! darn kids!", "free wifi", "no free wifi because brexit", NULL};
+/*list of ssid's to use*/
+char* names[] = {"Im watching you", "secret camera #%d", "Hi there", "I see you","MI5reconvanID#%d","loading..." , "no free wifi because Tories", "yell CARROT for password", "FBIvanID#%d", "Fellowship of the Ping", "get off my LAN! darn kids!", "free wifi", "no free wifi because brexit", NULL};
 int n = 0;
 void setup()
 {
@@ -9,9 +10,11 @@ randomSeed(analogRead(0));
 
 void loop()
 {
+   /*time (in ms) between changing ssid*/
    delay(300000);
    char buff[100];
    sprintf(buff, names[n], random(1000, 9999));
+   /*list of names to use password for*/
    if (names[n] == "yell CARROT for password" or names[n] == "BTE-IT" or names[n] == "no free wifi because Tories" or names[n] == "no free wifi because brexit")
    {
    WiFi.softAP(buff, "offmylan"); 
